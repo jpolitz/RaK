8d9bb840eb9028f2cc76e1adbf88479d08c63be6
Joe Gibbs Politz <joe@cs.brown.edu>
2012-07-23 21:56:14 -0400

    types

--- main.ml
+++ main.ml
@@ -1,41 +1,52 @@
 module F = Format
 module K = Kripke
-open K.StringKripke
 
 module Main = struct
+  type state = S | T | U
+  module StateOrd = struct
+    type t = state
+    let compare = Pervasives.compare
+  end;;
+  type prop = P
+  module Prop = struct
+    type t = prop
+  end;;
+  module SK = K.Kripke(StateOrd) (Prop)
+  open SK
 
   let fig21 =
-    let states = ["s"; "t"; "u"] in
+    let states = [S; T; U] in
     let k = [
-      [("s", "s"); ("s", "t"); ("t", "s"); ("t", "t"); ("u", "u")];
-      [("s", "s"); ("s", "u"); ("t", "t"); ("u", "s"); ("u", "u")]
+      [(S, S); (S, T); (T, S); (T, T); (U, U)];
+      [(S, S); (S, U); (T, T); (U, S); (U, U)]
     ] in
     let pi st _ = match st with
-      | "s"
-      | "u" -> true
-      | "t" -> false
-      | _ -> failwith "'splosions" in
+      | S
+      | U -> true
+      | T -> false in
     make states pi k
 
   let tests = [
-    (fun () -> F.printf "Should be true: %b\n"
-      (satisfies fig21 "s" (Prim "p")));
-    (fun () -> F.printf "Should be true: %b\n"
-      (satisfies fig21 "s" (Knows (1, Prim "p"))));
-    (fun () -> F.printf "Should be false: %b\n"
-      (satisfies fig21 "t" (Prim "p")));
-    (fun () -> F.printf "Should be true: %b\n"
-      (satisfies fig21 "s"
-        (And (Not (Knows (0, Prim "p")),
-              And (Knows (1, Prim "p"),
-                   And (Knows (0, Or (Knows (1, Prim "p"),
-                                      Knows (1, Not (Prim "p")))),
-                        Not (Knows (1, Not (Knows (0, Prim "p"))))))))))
+    ("Primitive", fun () ->
+      satisfies fig21 S (Prim P));
+    ("Knows", fun () ->
+      satisfies fig21 S (Knows (1, Prim P)));
+    ("NotPrim", fun () ->
+      not (satisfies fig21 T (Prim P)));
+    ("Page20", fun () ->
+      satisfies fig21 S
+       (And (Not (Knows (0, Prim P)),
+             And (Knows (1, Prim P),
+                  And (Knows (0, Or (Knows (1, Prim P),
+                                     Knows (1, Not (Prim P)))),
+                       Not (Knows (1, Not (Knows (0, Prim P)))))))))
   ]
 
 
   let main () =
-    List.iter (fun f -> f()) tests
+    List.iter (fun (desc, f) ->
+      F.printf "%s" desc;
+      if f() then F.printf " passed\n" else F.printf " failed\n") tests
 end;;
 Main.main ()
 
